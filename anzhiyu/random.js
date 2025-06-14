edf63ae8a169d9b4142c82d6ffc25ad969c3699b
pangxingzhong1983 <pangxingzhong@gmail.com>
2025-06-14 16:24:00 +0800

    Site updated: 2025-06-14 16:24:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/13/这是纵云梯的第一篇博文/","2025/06/13/hello-world/"];function toRandomPost(){
+var posts=["2025/06/13/这是折腾吧的一篇博文/","2025/06/14/这是一篇纵云梯的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
